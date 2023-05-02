c26c569ba19afe6070999dacd3e7ccca1b99c5a7
dimmerz92 <andrew.weymes@outlook.com>
2023-05-02 23:41:17 +0800

    add TLS to client.c

--- client.c
+++ client.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <netdb.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
 
 /*-------------------------
 | CONSTS
@@ -28,78 +26,133 @@ int main(int argc, char *argv[])
     // check arg length
     if (argc < 3)
     {
-        error("ERROR insufficient arguments");
+        fprintf(stderr, "Error insufficient arguments\n");
+        exit(1);
     }
 
-    // init vars
-    struct sockaddr_in serv_addr;
-    struct hostent *serv;
-    socklen_t serv_len = sizeof(serv_addr);
-    char buffer[MAX_BUFFER];
-    int sockfd, action;
-
-    // make socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    // Generate EC keys
+    EVP_PKEY_CTX *key_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
+    if (key_ctx == NULL)
     {
-        error("ERROR creating socket");
+        fprintf(stderr, "Error generating key context\n");
+        exit(1);
     }
 
-    // resolve hostname
-    if ((serv = gethostbyname(argv[1])) == NULL)
+    if (EVP_PKEY_keygen_init(key_ctx) <= 0)
     {
-        error("ERROR no such host");
+        fprintf(stderr, "Error generating keygen\n");
+        exit(1);
     }
 
-    // socket details
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(atoi(argv[2]));
-    bcopy((char *)serv->h_addr, (char*)&serv_addr.sin_addr.s_addr, serv->h_length);
+    if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(key_ctx, NID_X9_62_prime256v1) <= 0)
+    {
+        fprintf(stderr, "Error generating elliptic curve\n");
+        exit(1);
+    }
+
+    EVP_PKEY *keys;
+    if (EVP_PKEY_keygen(key_ctx, &keys) <= 0)
+    {
+        fprintf(stderr, "Error generating keys\n");
+        exit(1);
+    }
+
+    EVP_PKEY_CTX_free(key_ctx);
+
+    // Initialise OpenSSL SSL context object
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (ctx == NULL)
+    {
+        fprintf(stderr, "Error generating SSL context\n");
+        exit(1);
+    }
+
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+
+    // Initalise variables
+    int action;
+    char buffer[MAX_BUFFER], host_port[100];
+
+    strcpy(host_port, argv[1]);
+    strcat(host_port, ":");
+    strcat(host_port, argv[2]);
+
+    // Initialise OpenSSL socket
+    BIO *bio = BIO_new_ssl_connect(ctx);
+    if (bio == NULL)
+    {
+        fprintf(stderr, "Error initalising BIO socket\n");
+        exit(1);
+    }
+
+    // Initialise SSL
+    SSL *ssl;
+    if (BIO_get_ssl(bio, &ssl) <= 0)
+    {
+        fprintf(stderr, "Error initialising ssl\n");
+        exit(1);
+    }
+
+    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+    if(BIO_set_conn_hostname(bio, host_port) <= 0)
+    {
+        fprintf(stderr, "Error adding hostname\n");
+        exit(1);
+    }
     
-    // connect to server
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, serv_len) < 0)
+    // Connect to server
+    if (BIO_do_connect(bio) <= 0)
     {
-        error("ERROR connecting");
+        fprintf(stderr, "Error connecting to server\n");
+        BIO_free(bio);
+        exit(1);
+    }
+
+    // Perform TLS handshake
+    if (BIO_do_handshake(bio) <= 0)
+    {
+        fprintf(stderr, "Error on TLS handshake\n");
+        BIO_free(bio);
+        exit(1);
     }
 
     while (1)
     {
-        // send a message
+        // Client input
         memset(buffer, 0, MAX_BUFFER);
         fgets(buffer, MAX_BUFFER, stdin);
         if (ascii_buffer(buffer) < 0)
         {
-            close(sockfd);
-            error("ERROR input contains non-ASCII characters");
+            fprintf(stderr, "Error input contains non-ASCII characters\n");
+            BIO_free(bio);
+            exit(1);
         }
         
         // check ascii chars
-        if (write(sockfd, buffer, strlen(buffer)) < 0)
+        if (BIO_write(bio, buffer, strlen(buffer)) <= 0)
         {
-            close(sockfd);
-            error("ERROR writing");
+            fprintf(stderr, "Error writing to server\n");
+            BIO_free(bio);
+            exit(1);
         }
 
         // receive a message
-        memset(buffer, 0, MAX_BUFFER - 1);
-        if ((action = read(sockfd, buffer, MAX_BUFFER)) < 0)
+        memset(buffer, 0, MAX_BUFFER);
+        if ((action = BIO_read(bio, buffer, MAX_BUFFER)) <= 0)
         {
-            close(sockfd);
-            error("ERROR reading");
+            fprintf(stderr, "Error reading from server\n");
+            BIO_free(bio);
+            exit(1);
         }
 
         // check ascii chars
         if (ascii_buffer(buffer) < 0)
         {
-            close(sockfd);
-            error("ERROR input contains non-ASCII characters");
+            fprintf(stderr, "Error input contains non-ASCII characters\n");
+            BIO_free(bio);
+            exit(1);
         }
 
-        if (action == 0)
-        {
-            // server force disconnected
-            break;
-        }
         else if (strcmp(buffer, "ACK") == 0)
         {
             // send the value
@@ -109,29 +162,33 @@ int main(int argc, char *argv[])
             // check ascii chars
             if (ascii_buffer(buffer) < 0)
             {
-                close(sockfd);
-                error("ERROR input contains non-ASCII characters");
+                fprintf(stderr, "Error input contains non-ASCII characters\n");
+                BIO_free(bio);
+                exit(1);
             }
 
-            if (write(sockfd, buffer, strlen(buffer)) < 0)
+            if (BIO_write(bio, buffer, strlen(buffer)) <= 0)
             {
-                close(sockfd);
-                error("ERROR writing");
+                fprintf(stderr, "Error writing to server\n");
+                BIO_free(bio);
+                exit(1);
             }
 
             // receive a message
             memset(buffer, 0, MAX_BUFFER - 1);
-            if ((action = read(sockfd, buffer, MAX_BUFFER)) < 0)
+            if ((action = BIO_read(bio, buffer, MAX_BUFFER)) < 0)
             {
-                close(sockfd);
-                error("ERROR reading");
+                fprintf(stderr, "Error reading from server\n");
+                BIO_free(bio);
+                exit(1);
             }
 
             // check ascii chars
             if (ascii_buffer(buffer) < 0)
             {
-                close(sockfd);
-                error("ERROR input contains non-ASCII characters");
+                fprintf(stderr, "Error input contains non-ASCII characters\n");
+                BIO_free(bio);
+                exit(1);
             }
         }
 
@@ -145,7 +202,7 @@ int main(int argc, char *argv[])
     }
 
     // close socket
-    close(sockfd);
+    BIO_free(bio);
 
     return 0;
 }
@@ -153,13 +210,6 @@ int main(int argc, char *argv[])
 /*-------------------------
 | FUNCTIONS
 |-------------------------*/
-
-// quick handling for errors
-void error(char *msg)
-{
-    fprintf(stderr, "%s\n", msg);
-    exit(1);
-}
 
 // checks buffer contains only ASCII characters
 int ascii_buffer(char *buffer)
